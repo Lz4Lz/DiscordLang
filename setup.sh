8f07b98cd73d8f2a92f61f77a36e7c9d8f9e23da
Lz4Lz <59481770+Lz4Lz@users.noreply.github.com>
2022-02-11 17:09:48 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+git clone https://github.com/Lz4Lz/DiscordLang.git
+cd DiscordLang
+cd discordlang
+npm i discord.js@11
+npm i express
+npm i ejs
+npm i path
+npm i @vitalets/google-translate-api
+
+node .
